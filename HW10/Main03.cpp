2aad5aa265d10166ce6c2116d89b021effa073ae
EC2 Default User <ec2-user@ip-172-31-33-52.ec2.internal>
2022-11-19 03:15:17 +0000

    HW10&11&12

--- HW10/Main03.cpp
+++ HW10/Main03.cpp
@@ -16,6 +16,9 @@ int main()
     
     long N=30000;
     double epsilon =0.001;
+    
+    srand(1);
+    
     Option.PriceByMC(Model,N,epsilon);
     cout << "Arithmetic Asian Call by direct Monte Carlo = " << Option.GetPrice() << endl
         << "Error = " << Option.GetPricingError() << endl
@@ -26,9 +29,8 @@ int main()
     return 0;
 }
 /*
-Arithmetic Asian Call by direct Monte Carlo = 1.42987
-Error = 0.0120592
-delta = 0.524217
-gamma = 0.117831
+Arithmetic Asian Call by direct Monte Carlo = 1.41907
+Error = 0.0120537
+delta = 0.523935
+gamma = 0.126493
 */
-
